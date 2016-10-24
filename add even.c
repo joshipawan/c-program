e3d3fad49880161489e3110255a59ce125f2e26b
joshipawan <pawanjoshi211@gmail.com>
2016-10-24 10:47:16 +0545

    Add files via upload

---
+++ add even.c
@@ -0,0 +1,18 @@
+/*
+Student Name: Pawan joshi
+Roll no: 11
+Lab no: 
+Program: To print odd and even using terniary operator.
+Date: 24th october 2016
+*/
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+	int a;
+	printf("Enter the  number:\n");
+	scanf("%d",&a);
+	(a%2==0)?printf("The number you entered is Even\n"):printf("The number you entered is odd\n");
+	return 0;
+}
+	
