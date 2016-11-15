20e1d5ccf457b39070064fc0b7f702ee33bf317b
joshipawan <pawanjoshi211@gmail.com>
2016-11-15 10:15:16 +0545

    Add files via upload

---
+++ print your name.c
@@ -0,0 +1,19 @@
+/*
+Studen Name: PAwan Joshi
+Roll no. :16
+Lab no. :14
+Program: To print your name 10 times
+Date: 15 November 2016
+*/
+#include<stdio.h>
+#include<conio.h>
+int main(){
+	
+int i;
+
+for( i=1;i<=10;i++)
+{
+	printf("pAwan\n");
+}
+return 0;
+}
